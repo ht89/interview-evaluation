96143fe65569ffc1a85b400b5d201f78e6628457
Hung Tran <hungtran891989@gmail.com>
2023-08-20 07:05:30 +0700

    feat: implement section & question

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, CheckboxModule, CardModule],
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
-export class QuestionComponent {}
+export class QuestionComponent {
+  checked = false;
+}
--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PanelModule } from 'primeng/panel';
+import { QuestionComponent } from '../question/question.component';
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PanelModule, QuestionComponent],
   templateUrl: './section.component.html',
-  styleUrls: ['./section.component.scss']
+  styleUrls: ['./section.component.scss'],
 })
-export class SectionComponent {}
+export class SectionComponent {
+  @Input() header!: string;
+
+  @Input() toggleable = true;
+}
