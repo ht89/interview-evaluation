76e552c8109aaa784a2337a812fee1074e06749b
Hung Tran <hungtran891989@gmail.com>
2023-08-27 08:58:30 +0700

    feat: init firebase & auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
-import { SectionsComponent } from './sections/sections.component';
 import { ResultComponent } from './result/result.component';
+import { SectionsComponent } from './sections/sections.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    // App
     SectionsComponent,
     ResultComponent,
   ],
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBEtjDs9ALr2SJ0AtMxmB6QOtXRXg45WJM',
+    authDomain: 'interview-evaluation-2023.firebaseapp.com',
+    projectId: 'interview-evaluation-2023',
+    storageBucket: 'interview-evaluation-2023.appspot.com',
+    messagingSenderId: '282202423065',
+    appId: '1:282202423065:web:2e2530f57dad3a76d04f88',
+  },
+};
