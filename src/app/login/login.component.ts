82d116e330888ea7d8e25179b5758a9384c56f18
Hung Tran <hungtran891989@gmail.com>
2023-08-27 14:15:34 +0700

    feat: login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  selector: 'app-login',
+  selector: 'ie-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    CardModule,
+    ButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  submit(
+    isValid: boolean,
+    value: Partial<{ email: string | null; password: string | null }>
+  ): void {
+    console.log(isValid, value);
+
+    if (!isValid) return;
+
+    const { email, password } = value;
+
+    if (!email || !password) return;
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log(errorCode, errorMessage);
+      });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { User, getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  AuthGuard,
+  AuthPipe,
+  AuthPipeGenerator,
+} from '@angular/fire/auth-guard';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Route, provideRouter } from '@angular/router';
+import { map } from 'rxjs';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+const redirectUnauthorizedRequests: AuthPipe = map((user: User | null) => {
+  // if not logged in, redirect to `auth`
+  // if logged in and email verified, allow redirect
+  // if logged in and email not verified, redirect to `auth/verify`
+  // return !!user ? (user.emailVerified ? true : ['auth', 'verify']) : ['auth']
+
+  return !!user ? true : ['login'];
+});
+
+const authPipeGenerator: AuthPipeGenerator = () => redirectUnauthorizedRequests;
 
 const routes: Route[] = [
   {
@@ -14,12 +31,16 @@ const routes: Route[] = [
       import('./app/login/login.component').then((m) => m.LoginComponent),
   },
   {
-    path: '',
+    path: 'questions',
     loadComponent: () =>
       import('./app/sections/sections.component').then(
         (m) => m.SectionsComponent
       ),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: authPipeGenerator },
   },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
 ];
 
 bootstrapApplication(AppComponent, {
